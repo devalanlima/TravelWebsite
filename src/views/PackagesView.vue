e811cf4973528429989747fb4acd5c0417f45b1f
Alan Lima <dev.alanlima@gmail.com>
2023-06-10 16:58:22 -0300

    add / Vertical and Horizontal banners

--- src/views/PackagesView.vue
+++ src/views/PackagesView.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        Packages
+    <div class="grid grid-rows-3 grid-cols-4 gap-5 place-items-center w-full h-[730px] px-10">
+        <VerticalBanner class="row-span-2" />
+        <VerticalBanner class="row-span-2" />
+        <VerticalBanner class="row-span-2" />
+        <VerticalBanner class="row-span-2" />
+        <HorizontalBanner class="col-span-2" />
+        <HorizontalBanner class="col-span-2" />
     </div>
 </template>
+
+<script setup>
+import HorizontalBanner from '../components/HorizontalBanner.vue';
+import VerticalBanner from '../components/VerticalBanner.vue'
+</script>

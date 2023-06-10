1c570a5f152058109478379b27e956590a173d16
Alan Lima <dev.alanlima@gmail.com>
2023-06-10 13:10:18 -0300

    create / DestinationCard.vue component

---
+++ src/components/DestinationCard.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-96 h-44 rounded-xl bg-white/20 p-10 flex flex-col gap-2 font-Montserrat justify-center backdrop-blur border-2 border-transparent border-t-neutral-400/20 border-l-neutral-400/20 border-r-neutral-600/50 border-b-neutral-600/30">
+        <h3 class="font-semibold text-lg text-white/50">Rio de Janeiro</h3>
+        <p class="font-medium text-sm text-white/50 text-justify">Rio de Janeiro is a huge seaside city in Brazil, famed for its Copacabana and Ipanema beaches...</p>
+        <p class="font-bold text-sm text-white/50">Read More</p>
+    </div>
+</template>

5314efeaf2cf22504b38fe43b20dd19b07b8ad41
Alan Lima <dev.alanlima@gmail.com>
2023-06-10 16:55:40 -0300

    stylize / change width

--- src/components/DestinationCard.vue
+++ src/components/DestinationCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="w-96 h-44 rounded-xl bg-white/20 p-10 flex flex-col gap-2 font-Montserrat justify-center backdrop-blur border-2 border-transparent border-t-neutral-400/20 border-l-neutral-400/20 border-r-neutral-600/50 border-b-neutral-600/30">
-        <h3 class="font-semibold text-lg text-white/50">Rio de Janeiro</h3>
-        <p class="font-medium text-sm text-white/50 text-justify">Rio de Janeiro is a huge seaside city in Brazil, famed for its Copacabana and Ipanema beaches...</p>
-        <p class="font-bold text-sm text-white/50">Read More</p>
+    <div class="w-full h-44 rounded-xl bg-white/20 p-10 flex flex-col gap-2 font-Montserrat justify-center backdrop-blur border-2 border-t-neutral-400/20 border-l-neutral-400/20 border-r-neutral-600/50 border-b-neutral-600/30">
+        <h3 class="font-semibold text-lg text-white/70">Rio de Janeiro</h3>
+        <p class="font-medium text-sm text-white/70 text-justify">Rio de Janeiro is a huge seaside city in Brazil, famed for its Copacabana and Ipanema beaches...</p>
+        <p class="cursor-pointer font-bold w-fit text-sm text-white/70 after:h-[1px] after:w-0 after:rounded-full hover:after:w-full after:bg-white after:block after:transition-all"
+        >Read More</p>
     </div>
 </template>

bfddae847b9b23d148a32b3f4384297863387d03
Alan Lima <dev.alanlima@gmail.com>
2023-06-10 12:32:50 -0300

    create / ExploreScreen.vue component

---
+++ src/components/ExploreScreen.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="explore" class=" max-w-full h-[500px] pl-14 px-20 mx-auto mr-auto overflow-y-scroll grid place-items-center">
+        <div class="h-[500px] grid place-items-end pb-24">
+            <div class="font-Montserrat text-center flex flex-col gap-3" >
+                <h2 class="font-normal text-3xl text-white tracking-[0.8rem]">EXPLORE</h2>
+                <h2 class=" font-bold text-9xl text-white/75">BRAZIL</h2>
+            </div>
+        </div>
+        <div class="h-[500px] grid place-items-center">
+            <div>MAPA DO BRASIL BEM BONITO AQUI</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#explore {
+    scrollbar-width: thin;
+    scrollbar-color: white rgb(59, 59, 59);
+}
+
+#explore::-webkit-scrollbar {
+    width: 1px;
+}
+
+#explore::-webkit-scrollbar-track {
+    background: rgba(255, 255, 255, 0.342);
+}
+
+#explore::-webkit-scrollbar-thumb {
+    background-color: white;
+    border-radius: 20px;
+    outline: 1px solid rgba(255, 255, 255, 0.486);
+}
+
+</style>

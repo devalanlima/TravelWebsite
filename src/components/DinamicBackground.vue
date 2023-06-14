2d6eea5e6f4bce477f776158fc06b9a62f31a563
Alan Lima <dev.alanlima@gmail.com>
2023-06-14 18:12:49 -0300

    create / DinamicBackground.vue component

---
+++ src/components/DinamicBackground.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="bg-[#0000008c] w-screen h-screen fixed left-0 top-0"></div>
+        <img class="w-screen h-screen object-cover fixed -z-10 blur-sm" :src="atualState.selectedBackground" alt="bg">
+    </div>
+</template>
+
+<script setup>
+import { useAtualState } from '../stores/AtualState';
+const atualState = useAtualState()
+</script>

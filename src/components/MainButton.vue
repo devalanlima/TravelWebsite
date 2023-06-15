dac58dff336cd1428e842a1df6564dbce4b4787c
Alan Lima <dev.alanlima@gmail.com>
2023-06-15 11:57:08 -0300

    att / button text

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,3 +1,3 @@
 <template>
-    <button class="p-3 mr-5 self-center bg-white/90 hover:bg-white hover:scale-105 text-black rounded-xl">Confira</button>
+    <button class="p-3 mr-5 self-center bg-white/90 hover:bg-white hover:scale-105 text-black rounded-xl">Check</button>
 </template>

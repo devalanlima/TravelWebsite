b7013c5dd29428c6a3e2766d440789a15c5bef99
Alan Lima <dev.alanlima@gmail.com>
2023-06-15 10:02:24 -0300

    add / DinamicBackground.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
-
+import SocialMedia from './components/SocialMedia.vue';
+import DinamicBackground from './components/DinamicBackground.vue';
 </script>
 
 <template>
   <header>
     <NavBar/>
   </header>
-  <main>
+  <main class="max-w-screen-2xl px-10 mx-auto h-[730px] mt-10  relative">
+    <SocialMedia class="absolute bottom-52"/> 
     <RouterView />
   </main>
+  <DinamicBackground class="absolute left-0 top-0 -z-50"/>
 </template>

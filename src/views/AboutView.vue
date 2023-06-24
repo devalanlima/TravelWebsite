0c6411313a6bad9f1645ee59fdd16914270ed7b4
Alan Lima <dev.alanlima@gmail.com>
2023-06-24 12:37:19 -0300

    add / text to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,25 @@
 <template>
-  <div>about</div>
+  <div class="px-24 text-2xl flex flex-col gap-5 leading-10">
+    <p>
+      Welcome to my portfolio travel website! I'd like to
+      clarify that this website is solely a project and not an actual travel website. Its purpose is to demonstrate my
+      abilities in creating visually appealing and functional designs for various industries.
+    </p>
+    <p>
+      Please note that the content and imagery used on this site are for illustrative purposes only and do not represent
+      real travel destinations or services. The information provided here is purely fictional and should not be considered
+      accurate or reliable.
+    </p>
+    <p>
+      I would like to give credit to the talented designer Arshakir, whose incredible work can be found on the website
+      <a class="font-semibold text-neutral-300 hover:text-white" href="https://www.arshakir.com/" target="_blank">https://www.arshakir.com/</a>. Their designs have been a great inspiration for me, and I highly recommend visiting
+      their profile to explore more of their remarkable creations.
+    </p>
+    <p>
+      Thank you for visiting my portfolio website, and I hope you enjoy browsing through my projects and getting a glimpse
+      of my design capabilities. If you have any questions or would like to discuss potential collaborations, please feel
+      free to reach out to me on <a  class="font-bold text-neutral-300 hover:text-white" href="#">www.alanlima.dev</a>.
+    </p>
+  </div>
 </template>
 
